b323ee04cca533d0a611e2dcebcf907ff02eee36
Pascalvk <40421967+Pascalvk@users.noreply.github.com>
2024-10-20 16:04:28 +0200

    Created a simple Piece class

---
+++ ChessBlazorServer/Classes/ChessPiece.cs
@@ -0,0 +1,31 @@
+﻿namespace ChessBlazorServer.Classes
+{
+    public class ChessPiece
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public (string Character, string Number) Position { get; set; }
+        public bool isCaptured { get; set; }
+
+        public ChessPiece(string name, string color, string character, string number)
+        {
+            Name = name;
+            Color = color;
+            Position = (character, number);
+            isCaptured = false;
+        }
+
+
+        // Method to move a piece to a new position
+        public void Move(string newCharacter, string newNumber)
+        {
+            Position = (newCharacter, newNumber);
+        }
+
+        // Method to capture a piece
+        public void Capture()
+        {
+            isCaptured = true;
+        }
+    }
+}
