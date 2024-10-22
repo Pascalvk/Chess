665e322701f319f2854423ad8171f0eb35107bbc
Pascalvk <40421967+Pascalvk@users.noreply.github.com>
2024-10-22 15:41:26 +0200

    Added new functionality

--- ChessBlazorServer/Classes/ChessPiece.cs
+++ ChessBlazorServer/Classes/ChessPiece.cs
@@ -1,31 +1,48 @@
-﻿namespace ChessBlazorServer.Classes
+﻿using System.Linq;
+
+namespace ChessBlazorServer.Classes
 {
     public class ChessPiece
     {
         public string Name { get; set; }
         public string Color { get; set; }
         public (string Character, string Number) Position { get; set; }
-        public bool isCaptured { get; set; }
+        public bool IsCaptured { get; set; }
+        public bool HasMoved { get; set; }
 
         public ChessPiece(string name, string color, string character, string number)
         {
             Name = name;
             Color = color;
             Position = (character, number);
-            isCaptured = false;
+            IsCaptured = false;
+            HasMoved = false;
         }
 
 
         // Method to move a piece to a new position
-        public void Move(string newCharacter, string newNumber)
+        public void NewPosition(string newCharacter, string newNumber)
         {
             Position = (newCharacter, newNumber);
         }
 
-        // Method to capture a piece
-        public void Capture()
+        // Method to capture this piece
+        public void CaptureThisPiece()
         {
-            isCaptured = true;
+            IsCaptured = true;
         }
+
+        // method to mark a piece as moved
+        public void MarkAsMoved()
+        {
+            HasMoved = true;
+        }
+
+        // Method to check if a move is valid
+        public bool IsMoveValid(List<(int, int)> MoveList, (int, int) MoveToLocation)
+        {           
+            return MoveList.Contains(MoveToLocation);       
+        }
+
     }
 }
